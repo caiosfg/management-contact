42572b583b063867a744af323a58b0b2dd183acf
caiosfg <caio.sfg@gmail.com>
2024-12-22 10:57:06 -0300

    busca user pelo token

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { DetailUserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
 
@@ -10,5 +12,6 @@ router.get("/test", (request: Request, response: Response) => {
 
 router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
