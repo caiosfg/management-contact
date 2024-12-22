0a4bea901be7b705647ba64de149e64c4a08ed9a
caiosfg <caio.sfg@gmail.com>
2024-12-22 11:23:26 -0300

    remove user

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+import { RemoveUserController } from "./controllers/user/RemoveUserController";
 
 const router = Router();
 
@@ -10,8 +11,10 @@ router.get("/test", (request: Request, response: Response) => {
   return response.json({ ok: true });
 });
 
+// User
 router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+router.delete("/user/remove", new RemoveUserController().handle);
 
 export { router };
