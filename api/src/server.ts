e2c5865267eba3d424249fc23fab9759b4736c26
caiosfg <caio.sfg@gmail.com>
2024-12-24 11:39:24 -0300

    inserido swagger

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 import { router } from "./routes";
+import path from "path";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("v1", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
@@ -21,6 +28,12 @@ app.use(
     });
   }
 );
+
+app.get("/terms", (request: Request, response: Response) => {
+  return response.json({
+    message: "Termos de serviço",
+  });
+});
 
 app.listen(port, () => {
   console.log(`API rodando na porta : ${port}`);
